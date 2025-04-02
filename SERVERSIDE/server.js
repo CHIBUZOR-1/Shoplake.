edc3f3dd3c19a84adc18f994bff1a647babc2bcf
Chibuzor Amaechi <amaechihenrychibuzor@gmail.com>
2025-04-02 13:29:15 +0100

    checks and CSP fixes

--- SERVERSIDE/server.js
+++ SERVERSIDE/server.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
         contentSecurityPolicy: {
             directives: {
                 defaultSrc: ["'self'"],
-                scriptSrc: ["'self'", "https://embed.tawk.to",   "https://www.gstatic.com", "https://www.googleapis.com", "https://apis.google.com"],
+                scriptSrc: ["'self'", "https://embed.tawk.to", "https://cdn.jsdelivr.net",   "https://www.gstatic.com", "https://www.googleapis.com", "https://apis.google.com"],
                 styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "*.tawk.to"],
                 imgSrc: ["'self'", "data:", "https://www.gstatic.com", "blob:", "https://as2.ftcdn.net", "https://res.cloudinary.com", "https://cdn.jsdelivr.net", "*.tawk.to"],
                 connectSrc: ["'self'", "https://www.googleapis.com", "https://firebasestorage.googleapis.com", "https://identitytoolkit.googleapis.com", "blob:", "https://res.cloudinary.com", "*.tawk.to", "wss://*.tawk.to" ],
--- clientside/public/helpers/tawkCustom.js
+++ clientside/public/helpers/tawkCustom.js
@@ -1,10 +1,15 @@
-Tawk_API = Tawk_API || {};
-Tawk_API.onLoad = function() {
-    Tawk_API.minimize();
-    const backButton = document.querySelector('.tawk-icon-back');
-    if (backButton) backButton.style.display = 'none';
+// Ensure Tawk_API is defined
+if (window.Tawk_API) {
+    Tawk_API.onLoad = function () {
+        // Your custom logic
+        Tawk_API.minimize();
 
-    // Hide breadcrumbs
-    const breadcrumbs = document.querySelector('.tawk-breadcrumbs');
-    if (breadcrumbs) breadcrumbs.style.display = 'none';
-};
+        const backButton = document.querySelector('.tawk-icon-back');
+        if (backButton) backButton.style.display = 'none';
+
+        const breadcrumbs = document.querySelector('.tawk-breadcrumbs');
+        if (breadcrumbs) breadcrumbs.style.display = 'none';
+    };
+} else {
+    console.error("Tawk_API is not defined. Make sure the Tawk.to script is loaded before running this custom script.");
+}
